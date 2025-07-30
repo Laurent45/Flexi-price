fdbec7f8d4a094aa970ecc87e558bcdf440263e1
laurent.frederick <laurent.frederick@icloud.com>
2025-07-30 11:20:55 +0200

    Init commit

---
+++ src/test/java/org/capco/flexiprice/FlexiPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capco.flexiprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexiPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
